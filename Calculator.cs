c2ae67f53f142d736e1d11c3a09a9be6c049807c
Zackyam76 <zackyam76@students.amikom.ac.id>
2024-03-05 12:17:17 +0700

    Hasil Akhir

--- Calculator.cs
+++ Calculator.cs
@@ -6,10 +6,21 @@ class Calculator
     {
 		int a = 10;
 		int b = 6;
+		
+		Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, Penambahan(a, b));
+		Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}", a, b, Pengurangan(a, b));
+		
+		Console.WriteLine("\nTekan Sembarang key untuk keluar");
+		Console.Readkey();
 	}
 }
 
 static int penambahan(inta,int b)
 {
 	return a+ b;
+}
+
+static int Pengurangan(int a, int b)
+{
+	return a - b;
 }
